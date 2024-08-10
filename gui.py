3a25a6bfd03e714e7642043a0a32c05aab102bd7
andalib <andalib@gmail.com>
2024-08-10 23:10:48 +0530

    Implement the Add Button Day 17

--- gui.py
+++ gui.py
@@ -2,10 +2,28 @@ from Backend import functions
 import PySimpleGUI as pg
 
 label = pg.Text("Type in a todo")
-input_box = pg.InputText(tooltip='Enter todo')
+input_box = pg.InputText(tooltip='Enter todo', key='todo')
 add_button = pg.Button('Add')
 
-window = pg.Window('My To-do App', layout=[[label], [input_box, add_button]])
-window.read()
-print("hello")
+window = pg.Window('My To-do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 10))
+
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    match event:
+        case 'Add':
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case pg.WIN_CLOSED:
+            break
+
+print("Bye...")
 window.close()
