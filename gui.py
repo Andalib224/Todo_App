d3d9c0b3e91075f72bd13727f146858dcb56f3cd
andalib <andalib@gmail.com>
2024-08-13 19:13:54 +0530

    Implement Clock & theme in desktop application day 18.

--- gui.py
+++ gui.py
@@ -1,5 +1,11 @@
+import sys
+
 from Backend import functions
 import PySimpleGUI as pg
+import time
+
+pg.theme("Black")
+clock = pg.Text('', key="clock")
 
 label = pg.Text("Type in a todo")
 input_box = pg.InputText(tooltip='Enter todo', key='todo')
@@ -14,14 +20,13 @@ exit_button = pg.Button('Exit')
 
 
 window = pg.Window('My To-do App',
-                   layout=[[label], [input_box, add_button],
+                   layout=[[clock], [label], [input_box, add_button],
                            [list_box, edit_button, complete_button], [exit_button]],
                    font=('Helvetica', 10))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
+    event, values = window.read(timeout=200)
+    clock.update(value=time.strftime("%B %d, %Y %H:%M:%S"))
 
     match event:
         case 'Add':
@@ -33,25 +38,30 @@ while True:
             input_box.update(value='')
 
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
+            try:
+                todo_to_edit = values['todos'][0]
+                todos = functions.get_todos()
+                new_todo = values['todo'] + "\n"
 
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            list_box.update(todos)
-            functions.write_todos(todos)
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                list_box.update(todos)
+                functions.write_todos(todos)
+            except IndexError:
+                pg.popup("Please select an item first.", font=('Helvetica', 12))
 
         case 'Complete':
-            todo_to_complete = values['todos'][0]
+            try:
+                todo_to_complete = values['todos'][0]
 
-            todos = functions.get_todos()
-
-            index = todos.index(todo_to_complete)
-            todos.pop(index)
-            functions.write_todos(todos)
-            input_box.update(value='')
-            list_box.update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_complete)
+                todos.pop(index)
+                functions.write_todos(todos)
+                input_box.update(value='')
+                list_box.update(values=todos)
+            except IndexError:
+                pg.popup("Please select an item first.", font=('Helvetica', 12))
 
         case 'todos':
             selected_todo = values['todos'][0].replace('\n', '')
@@ -63,6 +73,4 @@ while True:
         case pg.WIN_CLOSED:
             break
 
-
-print("Bye...")
 window.close()
